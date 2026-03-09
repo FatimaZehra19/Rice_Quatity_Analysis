88c6f3eaa56de62d203606f142008d331438faff
Fatima Zehra <ffatimazehra059@gmail.com>
2026-03-10 03:34:24 +0500

    Initial project setup: dataset exploration and project structure

---
+++ Thesis_notes/Dataset_description.md
@@ -0,0 +1,6 @@
+Dataset = Rice Image Dataset from Kaggle 
+Number of classes: 5
+Classes in the dataset: [Arborio, Basmati, Ipsala, Jasmine, Karacadag]
+Number of images per class = 15000
+Total Number of Immages = 75,000
+Sample Image Shape(Height, Width, Channels): (250, 250, 3)
---
+++ src/Dataset_exploration.py
@@ -0,0 +1,43 @@
+import os
+import cv2
+import matplotlib.pyplot as plt
+
+# Path to the dataset
+dataset_path = r"c:\Users\Fatima\Desktop\Rice_thesis_project\Dataset\Rice_Image_Dataset"
+
+classes = os.listdir(dataset_path)
+print("Classes in the dataset:", classes)
+print("Number of classes:", len(classes))
+print()
+
+# Count the number of images in each class
+for cls in classes:
+    class_path = os.path.join(dataset_path, cls)
+    num_images = len(os.listdir(class_path))
+    print(f"Number of images in class '{cls}': {num_images}")
+
+print("\nDataset exploration completed. \n ")
+
+# Display a few sample images
+plt.figure(figsize=(10, 6))
+for i, cls in enumerate(classes):
+    class_path = os.path.join(dataset_path, cls)
+    sample_image = os.listdir(class_path)[0]  # Get the first image from the class
+    img_path = os.path.join(class_path, sample_image)
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert to RGB for displaying
+    plt.subplot(1, len(classes), i + 1)
+    plt.imshow(img)
+    plt.title(cls)
+    plt.axis('off')
+
+plt.tight_layout()
+plt.show()
+
+# Check the dimensions of a sample image
+sample_classes = classes[0]  # Choose the first class for checking
+sample_image = os.listdir(os.path.join(dataset_path, sample_classes))[0]  # Get the first image from the class
+sample_path = os.path.join(dataset_path, sample_classes, sample_image)
+img = cv2.imread(sample_path)
+
+print("Sample Image Shape(Height, Width, Channels):", img.shape)
